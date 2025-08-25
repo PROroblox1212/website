6c4a15d0d5754864214f5dcaaae5f378385b0ea7
PROroblox1212 <alexischarlebois499@gmail.com>
2025-08-25 18:02:00 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,6 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
+ctx.fillStyle = "blue";
+ctx.fillRect(canvas.width/2 - 25, canvas.height/2 - 25, 50 , 50);
